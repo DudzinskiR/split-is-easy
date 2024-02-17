c278a3668ca517605eb3c4bd2bfb9a905bdb54d2
Robert-Dudzinski <130515506+DudzinskiR@users.noreply.github.com>
2024-02-17 20:36:50 +0100

    Update public-home-footer.component.tsx

--- frontend/src/pages/public/home/components/segments/footer/public-home-footer.component.tsx
+++ frontend/src/pages/public/home/components/segments/footer/public-home-footer.component.tsx
@@ -1,7 +1,7 @@
 export const PublicHomeFooterSegment = () => {
   return (
     <div className="flex justify-center">
-      <div>Create by Robert Dudziński</div>
+      <div>Created by Robert Dudziński</div>
     </div>
   );
 };
