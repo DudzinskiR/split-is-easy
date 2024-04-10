2be01211cf871e9122c3d940d0b92c972b251ff0
Robert-Dudzinski <robert.dudzinski97@gmail.com>
2024-04-10 14:05:14 +0200

    Add custom-ping animation in tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -16,6 +16,16 @@ export default {
           150: "rgb(233,238,245)",
         },
       },
+      keyframes: {
+        "custom-ping": {
+          "0%": { opacity: 0, transform: "scale(0)" },
+          "10%": { opacity: 1 },
+          "100%": { opacity: 0, transform: "scale(1)" },
+        },
+      },
+      animation: {
+        "custom-ping": "custom-ping 6s ease-in-out infinite",
+      },
     },
   },
   plugins: [vitePluginRequire("@tailwindcss/forms")],
