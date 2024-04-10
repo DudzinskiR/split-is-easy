eb2d4775a43c84e9eebfe31e357c77e718d15f9b
Robert-Dudzinski <robert.dudzinski97@gmail.com>
2024-04-10 14:19:23 +0200

    Use new Vector2 class in mouse position hook

--- frontend/src/hooks/mouse-position/mouse-position.tsx
+++ frontend/src/hooks/mouse-position/mouse-position.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
-import { Vector2 } from "src/types";
+import { Vector2 } from "src/utils/math";
 
 export const useMousePosition = () => {
-  const [mousePosition, setMousePosition] = useState<Vector2>({ x: 0, y: 0 });
+  const [mousePosition, setMousePosition] = useState<Vector2>(Vector2.zero);
 
   useEffect(() => {
     const updateMousePosition = (event: MouseEvent) => {
-      setMousePosition({ x: event.clientX, y: event.clientY });
+      setMousePosition(new Vector2(event.clientX, event.clientY));
     };
     window.addEventListener("mousemove", updateMousePosition);
     return () => {
