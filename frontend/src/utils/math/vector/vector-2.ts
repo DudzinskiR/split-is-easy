12ec1ece10ca0483cba782098c5b6ae279168acf
Robert-Dudzinski <robert.dudzinski97@gmail.com>
2024-04-10 14:06:32 +0200

    Add Vector classes

---
+++ frontend/src/utils/math/vector/vector-2.ts
@@ -0,0 +1,46 @@
+export class Vector2 {
+  private _x: number;
+  private _y: number;
+
+  constructor();
+  constructor(vector: Vector2);
+  constructor(x: number, y: number);
+  constructor(arg1?: number | Vector2, y?: number) {
+    if (arg1 && arg1 instanceof Vector2) {
+      this._x = arg1.x;
+      this._y = arg1.y;
+    } else if (arg1 !== undefined && y !== undefined) {
+      this._x = arg1;
+      this._y = y;
+    } else {
+      this._x = 0;
+      this._y = 0;
+    }
+  }
+
+  public get x() {
+    return this._x;
+  }
+
+  public get y() {
+    return this._y;
+  }
+
+  public static get zero() {
+    return new Vector2(0, 0);
+  }
+
+  public add(vector: Vector2): void;
+  public add(x: number, y: number): void;
+  public add(arg1: number | Vector2, y?: number) {
+    if (arg1 instanceof Vector2) {
+      this._x += arg1.x;
+      this._y += arg1.y;
+    } else if (typeof arg1 === "number" && typeof y === "number") {
+      this._x += arg1;
+      this._y += y;
+    } else {
+      throw new Error("Invalid arguments provided to add function");
+    }
+  }
+}
---
+++ frontend/src/utils/math/vector/vector-3.ts
@@ -0,0 +1,60 @@
+export class Vector3 {
+  private _x: number;
+  private _y: number;
+  private _z: number;
+
+  constructor();
+  constructor(vector: Vector3);
+  constructor(x: number, y: number, z: number);
+  constructor(arg1?: number | Vector3, y?: number, z?: number) {
+    if (arg1 && arg1 instanceof Vector3) {
+      this._x = arg1.x;
+      this._y = arg1.y;
+      this._z = arg1.z;
+    } else if (arg1 !== undefined && y !== undefined && z !== undefined) {
+      this._x = arg1;
+      this._y = y;
+      this._z = z;
+    } else {
+      this._x = 0;
+      this._y = 0;
+      this._z = 0;
+    }
+  }
+
+  public get x() {
+    return this._x;
+  }
+
+  public get y() {
+    return this._y;
+  }
+
+  public get z() {
+    return this._z;
+  }
+
+  public static get zero() {
+    return new Vector3(0, 0, 0);
+  }
+
+  public add(x: number, y: number, z: number): void;
+  public add(vector: Vector3): void;
+  public add(arg1: number | Vector3, y?: number, z?: number) {
+    if (arg1 instanceof Vector3) {
+      this._x += arg1.x;
+      this._y += arg1.y;
+      this._z += arg1.z;
+    } else if (
+      typeof arg1 === "number" &&
+      typeof y === "number" &&
+      typeof z === "number"
+    ) {
+      this._x += arg1;
+      this._y += y;
+      this._z += z;
+    } else {
+      throw new Error("Invalid arguments provided to add function");
+    }
+  }
+}
