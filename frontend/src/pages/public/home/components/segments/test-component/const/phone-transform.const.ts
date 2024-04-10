f29d192cf4ba49022bc66f718e248d793d278b4b
Robert-Dudzinski <robert.dudzinski97@gmail.com>
2024-04-10 14:15:47 +0200

    Add phone3D position

---
+++ frontend/src/pages/public/home/components/segments/test-component/const/phone-transform.const.ts
@@ -0,0 +1,17 @@
+import { Transform3D } from "src/utils/math";
+
+export const startPhoneATransform = new Transform3D()
+  .setPosition(-2000, 0, 0)
+  .setRotate(10, 400, 50);
+
+export const startPhoneBTransform = new Transform3D()
+  .setPosition(1500, 20, 40)
+  .setRotate(10, -800, 50);
+
+export const finishPhoneATransform = new Transform3D()
+  .setPosition(-75, 0, 0)
+  .setRotate(10, 20, 0);
+
+export const finishPhoneBTransform = new Transform3D()
+  .setPosition(150, 20, 40)
+  .setRotate(10, -20, 0);
