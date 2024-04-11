2b2d3a8690dd03de79e1eaa2cb56070291617aec
Robert-Dudzinski <robert.dudzinski97@gmail.com>
2024-04-11 23:30:47 +0200

    Add set function in Vector2

--- frontend/src/utils/math/vector/vector-2.ts
+++ frontend/src/utils/math/vector/vector-2.ts
@@ -30,6 +30,20 @@ export class Vector2 {
     return new Vector2(0, 0);
   }
 
+  public set(vector: Vector2): void;
+  public set(x: number, y: number): void;
+  public set(arg1: number | Vector2, y?: number) {
+    if (arg1 instanceof Vector2) {
+      this._x = arg1.x;
+      this._y = arg1._y;
+    } else if (typeof arg1 === "number" && typeof y === "number") {
+      this._x = arg1;
+      this._y = y;
+    } else {
+      throw new Error("Invalid arguments provided to set function");
+    }
+  }
+
   public add(vector: Vector2): void;
   public add(x: number, y: number): void;
   public add(arg1: number | Vector2, y?: number) {
