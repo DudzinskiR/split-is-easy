bf9917756aca69a48a75b685e4bb856824838508
Robert-Dudzinski <robert.dudzinski97@gmail.com>
2024-04-15 15:42:13 +0200

    feat(PublicLayout): add new navbar

--- frontend/src/layouts/public/public.layout.tsx
+++ frontend/src/layouts/public/public.layout.tsx
@@ -4,7 +4,7 @@ import { Button } from "src/components";
 import { ButtonColor } from "src/enums";
 import PublicRoutes from "src/routes/public/public.route";
 import { PROJECT_NAME } from "src/utils/const";
-import { twMerge } from "tailwind-merge";
+import { twJoin, twMerge } from "tailwind-merge";
 
 const PublicLayout = () => {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -25,27 +25,27 @@ const PublicLayout = () => {
     <>
       <header
         className={twMerge(
-          "fixed w-full h-16 flex justify-center bg-transparent z-[100] duration-500",
-          isScrolled ? "shadow-md bg-white" : ""
+          "fixed w-full h-16 flex justify-center bg-transparent z-[100] duration-500 overflow-hidden",
+          isScrolled ? "shadow-lg" : ""
         )}
       >
+        <div
+          className={twJoin(
+            "absolute w-screen h-screen -z-10",
+            isScrolled ? "" : "hidden"
+          )}
+          style={{
+            background:
+              "linear-gradient(45deg, rgba(0,0,70,1) 0%, rgba(79,28,150,1) 33%, rgba(28,181,224,1) 100%)",
+            backgroundAttachment: "scroll",
+          }}
+        ></div>
         <div className="flex flex-row justify-around items-center max-w-7xl w-screen">
           <Link to="/">
-            <div
-              className={twMerge(
-                "text-2xl font-semibold",
-                isScrolled ? "text-black" : "text-white"
-              )}
-            >
+            <div className={twMerge("text-2xl font-semibold", "text-white")}>
               {PROJECT_NAME}
             </div>
           </Link>
-          <Button
-            className="w-[100px] text-lg duration-500"
-            color={isScrolled ? ButtonColor.PURPLE : "bg-white text-indigo-600"}
-            text="Login"
-            to="/login"
-          />
         </div>
       </header>
       <div className="relative flex justify-center overflow-x-hidden w-full">
