5efec673301efd9cc5dfb866370c92791a13919f
Robert-Dudzinski <robert.dudzinski97@gmail.com>
2024-04-16 20:10:08 +0200

    feat(FooterSegment): add justify-center

--- frontend/src/pages/public/home/components/segments/footer-segment/footer-segment.tsx
+++ frontend/src/pages/public/home/components/segments/footer-segment/footer-segment.tsx
@@ -1,6 +1,6 @@
 export const FooterSegment = () => {
   return (
-    <div className="h-[50px] flex items-center text-lg text-slate-600">
+    <div className="h-[50px] flex items-center justify-center text-lg text-slate-600">
       Created by Robert Dudziński
     </div>
   );
