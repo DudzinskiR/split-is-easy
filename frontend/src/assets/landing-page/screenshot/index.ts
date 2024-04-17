87bf6b1ae6fce0188090e021e6bb389c75663524
Robert-Dudzinski <robert.dudzinski97@gmail.com>
2024-04-17 02:53:14 +0200

    add new screenshot

--- frontend/src/assets/landing-page/screenshot/index.ts
+++ frontend/src/assets/landing-page/screenshot/index.ts
@@ -3,6 +3,7 @@ import LoadingPageScreenshot2 from "./landing-page-screenshot-2.png";
 import LoadingPageScreenshot3 from "./landing-page-screenshot-3.png";
 import LoadingPageScreenshot4 from "./landing-page-screenshot-4.png";
 import LoadingPageScreenshot5 from "./landing-page-screenshot-5.png";
+import LoadingPageScreenshot6 from "./landing-page-screenshot-6.png";
 
 export {
   LoadingPageScreenshot1,
@@ -10,4 +11,5 @@ export {
   LoadingPageScreenshot3,
   LoadingPageScreenshot4,
   LoadingPageScreenshot5,
+  LoadingPageScreenshot6,
 };
