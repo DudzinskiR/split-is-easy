e85dcd6b6ea00f4534403c65c40bc677e443c414
Robert-Dudzinski <robert.dudzinski97@gmail.com>
2024-04-17 02:59:23 +0200

    feat(PublicHome): add ScrollToButton component

--- frontend/src/pages/public/home/public-home.page.tsx
+++ frontend/src/pages/public/home/public-home.page.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useRef } from "react";
+import { ReactNode, useEffect, useRef, useState } from "react";
 
 import {
   AboutSegment,
@@ -9,6 +9,9 @@ import {
   MainSegment,
 } from "./components/segments";
 import { PublicNavbar } from "./components";
+import { ScrollToButton } from "src/components/scroll-to-button";
+import { FaArrowDown, FaArrowUp } from "react-icons/fa";
+import { twJoin } from "tailwind-merge";
 
 export type NavbarElement = {
   component: ReactNode;
@@ -26,10 +29,40 @@ const segments: NavbarElement[] = [
 
 const PublicHomePage = () => {
   const segmentsRef = useRef<HTMLDivElement[]>([]);
+  const [isScrollToTopButtonShow, setScrollToTopButtonShow] = useState(false);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      setScrollToTopButtonShow(scrollTop > window.screenY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
     <>
-      <PublicNavbar elements={segments} segmentsRef={segmentsRef} />
+      <PublicNavbar elements={segments} segmentsRef={segmentsRef} height={60} />
+      <ScrollToButton
+        positionToScroll={
+          (segmentsRef.current[1]?.getBoundingClientRect().top || 0) +
+          window.scrollY -
+          60
+        }
+        className="absolute absolute-center-x top-[85vh] z-10 animate-bounce"
+        icon={<FaArrowDown />}
+      />
+      <ScrollToButton
+        positionToScroll={0}
+        className={twJoin(
+          "fixed right-[50px] top-[85vh] z-10",
+          isScrollToTopButtonShow ? "opacity-100" : "opacity-0"
+        )}
+        icon={<FaArrowUp />}
+      />
       <div className="flex flex-col w-full">
         {segments.map((item, index) => (
           <div
