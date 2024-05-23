f651debc52d74af5139e231474ac2c4a251d32ce
Robert-Dudzinski <robert.dudzinski97@gmail.com>
2024-05-23 14:03:34 +0200

    feat: Expand Vector3 class

--- frontend/src/utils/math/vector/vector-3.ts
+++ frontend/src/utils/math/vector/vector-3.ts
@@ -1,5 +1,3 @@
-import { Vector2 } from "./vector-2";
-
 export class Vector3 {
   private _x: number;
   private _y: number;
@@ -36,27 +34,85 @@ export class Vector3 {
     return this._z;
   }
 
-  public set x(x: number) {
-    this._x = x;
-  }
-  public set y(y: number) {
-    this._y = y;
-  }
-  public set z(z: number) {
-    this._z = z;
-  }
-
   public static get zero() {
     return new Vector3(0, 0, 0);
   }
 
-  public add(x: number, y: number, z: number): void;
-  public add(vector: Vector3): void;
+  public static get up() {
+    return new Vector3(0, 1, 0);
+  }
+
+  public static get down() {
+    return new Vector3(0, -1, 0);
+  }
+
+  public static get right() {
+    return new Vector3(1, 0, 0);
+  }
+
+  public static get left() {
+    return new Vector3(-1, 0, 0);
+  }
+
+  public static get forward() {
+    return new Vector3(0, 0, 1);
+  }
+
+  public static get backward() {
+    return new Vector3(0, 0, -1);
+  }
+
+  public static distance(
+    vectorA: Vector3 | { x: number; y: number; z: number },
+    vectorB: Vector3 | { x: number; y: number; z: number }
+  ) {
+    if (vectorA instanceof Vector3) {
+      if (vectorB instanceof Vector3) {
+        return vectorA.distanceTo(vectorB);
+      } else {
+        return vectorA.distanceTo(vectorB.x, vectorB.y, vectorB.z);
+      }
+    } else {
+      if (vectorB instanceof Vector3) {
+        return new Vector3(vectorA.x, vectorA.y, vectorA.z).distanceTo(vectorB);
+      } else {
+        return new Vector3(vectorA.x, vectorA.y, vectorA.z).distanceTo(
+          vectorB.x,
+          vectorB.y,
+          vectorB.z
+        );
+      }
+    }
+  }
+
+  public set(vector: Vector3): void;
+  public set(x: number, y: number, z: number): void;
+  public set(arg1: number | Vector3, y?: number, z?: number) {
+    if (arg1 instanceof Vector3) {
+      this._x = arg1.x;
+      this._y = arg1.y;
+      this._z = arg1.z;
+    } else if (
+      typeof arg1 === "number" &&
+      typeof y === "number" &&
+      typeof z === "number"
+    ) {
+      this._x = arg1;
+      this._y = y;
+      this._z = z;
+    } else {
+      throw new Error("Invalid arguments provided to set function");
+    }
+  }
+
+  public add(vector: Vector3): Vector3;
+  public add(x: number, y: number, z: number): Vector3;
   public add(arg1: number | Vector3, y?: number, z?: number) {
     if (arg1 instanceof Vector3) {
       this._x += arg1.x;
       this._y += arg1.y;
       this._z += arg1.z;
+      return this;
     } else if (
       typeof arg1 === "number" &&
       typeof y === "number" &&
@@ -65,19 +121,19 @@ export class Vector3 {
       this._x += arg1;
       this._y += y;
       this._z += z;
+      return this;
     } else {
       throw new Error("Invalid arguments provided to add function");
     }
   }
 
-  public subtract(x: number, y: number, z: number): Vector3;
-  public subtract(vector: Vector3): Vector3;
-  public subtract(arg1: number | Vector3, y?: number, z?: number) {
+  public sub(vector: Vector3): Vector3;
+  public sub(x: number, y: number, z: number): Vector3;
+  public sub(arg1: number | Vector3, y?: number, z?: number) {
     if (arg1 instanceof Vector3) {
       this._x -= arg1.x;
       this._y -= arg1.y;
       this._z -= arg1.z;
-
       return this;
     } else if (
       typeof arg1 === "number" &&
@@ -87,20 +143,61 @@ export class Vector3 {
       this._x -= arg1;
       this._y -= y;
       this._z -= z;
-
       return this;
     } else {
-      throw new Error("Invalid arguments provided to add function");
+      throw new Error("Invalid arguments provided to sub function");
     }
   }
 
-  public toVector2() {
-    return new Vector2(this._x, this._z);
+  public mul(vector: Vector3): Vector3;
+  public mul(x: number, y: number, z: number): Vector3;
+  public mul(value: number): Vector3;
+  public mul(arg1: number | Vector3, y?: number, z?: number) {
+    if (arg1 instanceof Vector3) {
+      this._x *= arg1.x;
+      this._y *= arg1.y;
+      this._z *= arg1.z;
+      return this;
+    } else if (
+      typeof arg1 === "number" &&
+      typeof y === "number" &&
+      typeof z === "number"
+    ) {
+      this._x *= arg1;
+      this._y *= y;
+      this._z *= z;
+      return this;
+    } else if (typeof arg1 === "number" && y === undefined && z === undefined) {
+      this._x *= arg1;
+      this._y *= arg1;
+      this._z *= arg1;
+      return this;
+    } else {
+      throw new Error("Invalid arguments provided to mul function");
+    }
   }
 
-  public toString(fractionDigits?: number) {
-    return `Vector3(${this._x.toFixed(fractionDigits)}, ${this._y.toFixed(
-      fractionDigits
-    )}, ${this._z.toFixed(fractionDigits)})`;
+  public distanceTo(vector: Vector3): number;
+  public distanceTo(x: number, y: number, z: number): number;
+  public distanceTo(arg1: number | Vector3, y?: number, z?: number) {
+    if (arg1 instanceof Vector3) {
+      return Math.sqrt(
+        (arg1.x - this.x) ** 2 + (arg1.y - this.y) ** 2 + (arg1.z - this.z) ** 2
+      );
+    } else if (
+      typeof arg1 === "number" &&
+      typeof y === "number" &&
+      typeof z === "number"
+    ) {
+      return Math.sqrt(
+        (arg1 - this.x) ** 2 + (y - this.y) ** 2 + (z - this.z) ** 2
+      );
+    } else {
+      throw new Error("Invalid arguments provided to distanceTo function");
+    }
+  }
+
+  public toArray(): [number, number, number] {
+    return [this._x, this._y, this._z];
   }
 }
