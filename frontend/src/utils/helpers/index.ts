015b2c3ac2b7466c5b3db21403aae63161acf741
Robert-Dudzinski <robert.dudzinski97@gmail.com>
2024-05-23 14:02:12 +0200

    feat: Add lerp function

--- frontend/src/utils/helpers/index.ts
+++ frontend/src/utils/helpers/index.ts
@@ -2,6 +2,7 @@ export * from "./capitalize-first-letter";
 export * from "./check-is-within-error-margin";
 export * from "./date-formatter";
 export * from "./find-value";
+export * from "./lerp";
 export * from "./rgba-to-hex";
 export * from "./round-to-places";
 export * from "./to-radians";
