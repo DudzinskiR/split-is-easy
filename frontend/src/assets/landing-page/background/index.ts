c323a90c35faf074f0771d4826f470ad0bce1192
Robert-Dudzinski <robert.dudzinski97@gmail.com>
2024-05-23 14:09:32 +0200

    feat: Add SVG background files for landing page

---
+++ frontend/src/assets/landing-page/background/index.ts
@@ -0,0 +1,4 @@
+import Background1 from "./background1.svg?react";
+import Background2 from "./background2.svg?react";
+import Background3 from "./background3.svg?react";
+export { Background1, Background2, Background3 };
